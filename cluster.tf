d79ee64fc9d4d7615d0f686fbcb405e4a19261c9
Conrad Olega <conradolega@gmail.com>
2019-06-23 16:49:18 +0800

    Add one node

--- cluster.tf
+++ cluster.tf
@@ -4,3 +4,9 @@ resource "google_container_cluster" "selenium_grid" {
   remove_default_node_pool = true
   initial_node_count       = 1
 }
+
+resource "google_container_node_pool" "selenium" {
+  name       = "selenium"
+  cluster    = "${google_container_cluster.selenium_grid.name}"
+  node_count = 1
+}
